bd6f4fcb695d4141a3f671f6666ea0ce88f99929
zezemanolo <manoloish@gmail.com>
2018-04-08 14:13:32 -0700

    some spring cleaning to revive project

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Grid, Image, Jumbotron, Row, Col} from 'react-bootstrap'
+import { Button, Grid, Image, Jumbotron, Row, Col } from 'react-bootstrap';
 import ConnectWithMe from './ConnectWithMe';
 
 export default class Body extends Component {
@@ -9,9 +9,9 @@ export default class Body extends Component {
         <Jumbotron>
           <Grid>
             <Col xs={12} md={8}>
-              <h1>Hello, world!</h1>
-              <p>This is Zeze's portfolio page</p>
               <ConnectWithMe />
+              <h1>Hello, world!</h1>
+              <p>This is Zezes portfolio page</p>
             </Col>
             <Col xs={6} md={4}>
               <Image src="./assets/runningze.jpg" responsive />
@@ -22,24 +22,36 @@ export default class Body extends Component {
           <Row>
             <Col md={4}>
               <h2>Experience</h2>
-              <p>I am a Software Engineer with a background in Sales and Business Development across a variety of industries.</p>
-              <p><Button>View details »</Button></p>
+              <p>
+                I am a Software Engineer with a background in Sales and Business
+                Development across a variety of industries.
+              </p>
+              <p>
+                <Button>View details »</Button>
+              </p>
             </Col>
             <Col md={4}>
               <h2>About Me</h2>
-              <p>I love to run and have completed 4 full marathons. I'm a lifelong learner and student of Philosophy. </p>
-              <p><Button>View details »</Button></p>
+              <p>
+                I love to run and have completed 4 full marathons. Im a lifelong
+                learner and student of Philosophy.{' '}
+              </p>
+              <p>
+                <Button>View details »</Button>
+              </p>
             </Col>
             <Col md={4}>
               <h2>Projects</h2>
-              <p>
+              <div>
                 <ul>
                   <li>GrannyMeds</li>
                   <li>MarkdownParsingApp</li>
                   <li>TetrisReact</li>
                 </ul>
+              </div>
+              <p>
+                <Button>View details »</Button>
               </p>
-              <p><Button>View details »</Button></p>
             </Col>
           </Row>
         </Grid>
--- src/components/ConnectWithMe.js
+++ src/components/ConnectWithMe.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, ButtonGroup,ButtonToolbar, Modal } from 'react-bootstrap';
+import { Button, ButtonGroup, ButtonToolbar, Modal } from 'react-bootstrap';
 import SocialIcons from './SocialMediaIcons';
 
 export default class ConnectWithMe extends Component {
@@ -10,32 +10,41 @@ export default class ConnectWithMe extends Component {
 
     this.state = {
       open: false
-    }
+    };
   }
 
   closeModal() {
-    this.setState({open: false})
+    this.setState({ open: false });
   }
 
   openModal() {
-    this.setState({open: true})
+    this.setState({ open: true });
   }
 
   render() {
     return (
       <div>
-        <Button bsStyle='primary' bsSize='large' onClick={this.openModal} active>Connect »</Button>
+        <Button
+          bsStyle="primary"
+          bsSize="large"
+          onClick={this.openModal}
+          active
+        >
+          Connect »
+        </Button>
         <Modal animation show={this.state.open} onHide={this.closeModal}>
-          <Modal.Header style={{textAlign: "center"}} closeButton>
+          <Modal.Header style={{ textAlign: 'center' }} closeButton>
             <Modal.Title>Connect with me!</Modal.Title>
           </Modal.Header>
-          <Modal.Body style={{textAlign: "center"}}>
-            <SocialIcons style={{ height: 50, width: 50 }}/>
+          <Modal.Body style={{ textAlign: 'center' }}>
+            <SocialIcons style={{ height: 50, width: 50 }} />
           </Modal.Body>
           <Modal.Footer>
             <ButtonToolbar>
               <ButtonGroup vertical block>
-                <Button bsSize='large' onClick={this.closeModal} active>Close</Button>
+                <Button bsSize="large" onClick={this.closeModal} active>
+                  Close
+                </Button>
               </ButtonGroup>
             </ButtonToolbar>
           </Modal.Footer>
